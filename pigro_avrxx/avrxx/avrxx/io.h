27ba97c0f3b330537d5cb32dcf646cb46463efe0
Alex V. Zolotov <shade@shamangrad.net>
2020-05-31 16:45:30 +0300

    cached(), forced(), raw() wrappers

--- pigro_avrxx/avrxx/avrxx/io.h
+++ pigro_avrxx/avrxx/avrxx/io.h
@@ -59,6 +59,101 @@ namespace avr
         while ( true ) sleep();
     }
 
+    template <typename T> class cached_io;
+    template <typename T> class forced_io;
+    template <typename T> class raw_io;
+
+    template <typename T>
+    class cached_io
+    {
+    private:
+
+        T &var;
+
+    public:
+
+        T &ref() const { return var; }
+
+        constexpr cached_io(volatile T &r): var((T &)r) { }
+        constexpr cached_io(const cached_io &) = default;
+        constexpr cached_io(cached_io &&) = delete;
+        template <class IO> constexpr cached_io(const IO &io): var(io.ref) { }
+
+        T read() const
+        {
+            return var;
+        }
+
+        void write(T value) const
+        {
+            var = value;
+        }
+
+    };
+
+    template <typename T>
+    class forced_io
+    {
+    private:
+
+        volatile T &var;
+
+    public:
+
+        volatile T &ref() const { return var; }
+
+        constexpr forced_io(volatile T &r): var(r) { }
+        constexpr forced_io(const forced_io &) = default;
+        constexpr forced_io(forced_io &&) = delete;
+        template <class IO> constexpr forced_io(const IO &io): var(io.ref) { }
+
+        T read() const
+        {
+            return var;
+        }
+
+        void write(T value) const
+        {
+            var = value;
+        }
+
+    };
+
+    template <typename T>
+    class raw_io
+    {
+    private:
+
+        volatile T &var;
+
+    public:
+
+        volatile T &ref() const { return var; }
+
+        constexpr raw_io(volatile T &r): var(r) { }
+        constexpr raw_io(const raw_io &) = default;
+        constexpr raw_io(raw_io &&) = delete;
+        template <class IO> constexpr raw_io(const IO &io): var(io.ref) { }
+
+        T read() const
+        {
+            const uint8_t port = reinterpret_cast<int>(&var) - 0x20;
+            // здесь не нужен ни "memory" ни __volatile__
+            uint8_t value;
+            __asm__  ( "in %0, %1" : "=r"(value) : "I"(port) );
+            return value;
+
+        }
+
+        void write(T value)
+        {
+            const uint8_t port = reinterpret_cast<int>(&var) - 0x20;
+            // здесь не нужен ни "memory" ни __volatile__
+            __asm__  ( "out %1, %0" :: "r"(value), "I"(port) );
+        }
+
+    };
+
     template <typename T>
     class pin
     {
@@ -88,6 +183,90 @@ namespace avr
         }
 
     };
+
+    template <typename T>
+    auto cached(volatile T &r)
+    {
+        return cached_io(r);
+    }
+
+    template <class T>
+    auto cached(const T &io)
+    {
+        return cached_io(io.ref());
+    }
+
+    template <typename T>
+    auto forced(volatile T &r)
+    {
+        return forced_io(r);
+    }
+
+    template <class T>
+    auto forced(const T &io)
+    {
+        return forced_io(io.ref());
+    }
+
+    template <typename T>
+    auto raw(volatile T &r)
+    {
+        return raw_io(r);
+    }
+
+    template <class T>
+    auto raw(const T &io)
+    {
+        return raw_io(io.ref());
+    }
+
+    template <typename T>
+    T read(volatile T &r)
+    {
+        return forced(r).read();
+    }
+
+    template <class T>
+    T read(const T &io)
+    {
+        return forced(io.ref()).read();
+    }
+
+    template <typename T>
+    void write(volatile T &r, T value)
+    {
+        forced(r).write(value);
+    }
+
+    template <class T>
+    void write(const T &io, T value)
+    {
+        forced(io).write(value);
+    }
+
+    template <typename T>
+    T raw_read(volatile T &r)
+    {
+        return raw(r).read();
+    }
+
+    template <class T>
+    T raw_read(const T &io)
+    {
+        return raw(io).read();
+    }
+
+    template <typename T, typename T2>
+    void raw_write(volatile T &r, T2 value)
+    {
+        raw(r).write(value);
+    }
+
+    template <class T>
+    void raw_write(const T &io, T value)
+    {
+        raw(io).write(value);
+    }
 
     template <typename T>
     class bitfield
